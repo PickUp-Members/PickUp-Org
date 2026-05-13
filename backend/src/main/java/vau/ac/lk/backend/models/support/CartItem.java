602da14b4ee92e3479ef9c008129a575e0709bee
Kavindu - Dissanayake <induwara0460@gmail.com>
2026-05-13 11:19:33 +0530

    Add files via upload

---
+++ backend/src/main/java/vau/ac/lk/backend/models/support/CartItem.java
@@ -0,0 +1,16 @@
+package vau.ac.lk.backend.models.support;
+
+public class CartItem {
+    private String productId;
+    private int quantity;
+
+    public CartItem() {}
+    public CartItem(String productId, int quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+    public String getProductId() { return productId; }
+    public void setProductId(String productId) { this.productId = productId; }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+}
